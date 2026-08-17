6c23598dd6109bd258f19cbb2031a4862a4cff07
linkonroy <65219909+linkonroy@users.noreply.github.com>
2026-08-18 00:23:54 +0600

    Add arithmetic operators example

---
+++ 01-basics/operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 20;
+    int b = 6;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    printf("Addition       : %d\n", a + b);
+    printf("Subtraction    : %d\n", a - b);
+    printf("Multiplication : %d\n", a * b);
+    printf("Division       : %d\n", a / b);
+    printf("Remainder      : %d\n", a % b);
+
+    return 0;
+}
