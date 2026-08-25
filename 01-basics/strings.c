7da3382447ca4c413e4771c765e9f5ebdaa582fc
linkonroy <65219909+linkonroy@users.noreply.github.com>
2026-08-26 00:15:01 +0600

    Add strings example

---
+++ 01-basics/strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name[] = "Link";
+
+    printf("Name: %s\n", name);
+    printf("First character: %c\n", name[0]);
+    printf("Second character: %c\n", name[1]);
+    printf("Third character: %c\n", name[2]);
+    printf("Fourth character: %c\n", name[3]);
+
+    return 0;
+}
